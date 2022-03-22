124bdfc48d67e2fc801a9366faf395d5240ade00
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-03-22 17:14:29 +0300

    Add Git init

--- AR Glasses/ContentView.swift
+++ AR Glasses/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Владимир Ладыгин on 22.03.2022.
 //
-
+import UIKit
 import SwiftUI
 import RealityKit
 
@@ -20,11 +20,6 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
         
         return arView
         
