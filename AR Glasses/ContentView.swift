69a029c5f65ceec84967d8d3ecb3709d2bae253e
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-03-25 18:00:03 +0300

    Add rename functions and generate bridge function

--- AR Glasses/ContentView.swift
+++ AR Glasses/ContentView.swift
@@ -16,6 +16,39 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    func createCircle(x: Float = 0, y: Float = 0, z: Float = 0) -> Entity {
+        //Create circle mesh
+        let circle = MeshResource.generateBox(size: 0.05, cornerRadius: 0.025)
+        
+        //Create material
+        let material = SimpleMaterial(color: .blue, isMetallic: true)
+        
+        //Create Entity
+        let circleEntity = ModelEntity(mesh: circle, materials: [material])
+        circleEntity.position = SIMD3(x, y, z)
+        circleEntity.scale.x = 1.1
+        circleEntity.scale.z = 0.01
+        
+        return circleEntity
+        
+    }
+    func createBridge(x: Float = 0, y: Float = 0, z: Float = 0) -> Entity {
+        //Create bridge mesh
+        let bridge = MeshResource.generateBox(size: 0.005)
+        
+        //Create material
+        let material = SimpleMaterial(color: .lightGray, isMetallic: true)
+        
+        //Create Entity
+        let bridgeEntity = ModelEntity(mesh: bridge, materials: [material])
+        bridgeEntity.position = SIMD3(x, y, z)
+        bridgeEntity.scale.x = 5
+        bridgeEntity.scale.z = 0.2
+        
+        return bridgeEntity
+        
+    }
+    
     func makeUIView(context: Context) -> ARView {
         
         //Create AR view
@@ -30,9 +63,19 @@ struct ARViewContainer: UIViewRepresentable {
         let configuration = ARFaceTrackingConfiguration()
         configuration.isLightEstimationEnabled = true
         
+        //Create face anchor
+        let faceAnchor = AnchorEntity(.face)
+        
+        //Add box to the face ancor
+        faceAnchor.addChild(createCircle(x: 0.035, y: 0.025, z: 0.06))
+        faceAnchor.addChild(createCircle(x: -0.035, y: 0.025, z: 0.06))
+        faceAnchor.addChild(createBridge(y: 0.035, z: 0.06))
         
         //run face tracking session
         arView.session.run(configuration, options: [])
+        
+        //add face anchor to the scene
+        arView.scene.anchors.append(faceAnchor)
         
         return arView
     }
